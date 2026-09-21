6ac73552236435b369174b5ab6281a466da871bf
Felicidad Cruz <felicidadcruz89@gmail.com>
2026-09-21 00:29:33 -0300

    Finalizados componentes, modularizacion y estilos de la practica

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
-import { Navbar } from './components/Navbar/Navbar';
-import { MainContent } from './components/MainContent/MainContent';
-import { Footer } from './components/Footer/Footer';
+import  Navbar   from './components/Navbar/Navbar';
+import  MainContent  from './components/MainContent/MainContent';
+import  Footer  from './components/Footer/Footer';
 
 function App() {
   return (
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,12 +1,17 @@
-import styles from './Navbar.module.css';
+import estilos from './Navbar.module.css';
 
-export function Navbar() {
+function Navbar() {
   return (
-    <header className={styles.navbar}>
-      <div className={styles.logoContainer}>
-        <img src="/Logo.png" alt="Logo RT Bumpers y Gym Lucía" className={styles.logo} />
-        <h1 className={styles.title}>Gimnasios RT Bumpers & Gym Lucía</h1>
-      </div>
+    <header className={estilos.barra}>
+      <img src="/Logo.png" alt="Logo del sistema" className={estilos.logo} /> 
+      <h1 className={estilos.titulo}>Gimnasios RT Burpers y El Gym Lucía</h1>
+      <nav className={estilos.menu}>
+        <a href="#">Inicio</a>
+        <a href="#">Módulos</a>
+        <a href="#">Equipo</a>
+      </nav>
     </header>
   );
 }
+
+export default Navbar;
